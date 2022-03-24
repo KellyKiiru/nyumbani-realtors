de80be073211624f2e96694a4a3ca8baf821aa64
lydiah1wachira <lydiahcedric@gmail.com>
2022-03-24 11:56:07 +0300

    create object for the apartment buying and selling prices and add styles to the form.

---
+++ script/listings.js
@@ -0,0 +1,24 @@
+// back end logic
+const house = {
+
+  studio: {
+    rent: 50000,
+    buy: 1500000,
+  },
+
+  onebed: {
+    rent: 100000,
+    buy: 3500000,
+  },
+
+  threebed: {
+    rent:200000,
+    buy:6000000,
+  },
+
+  fourbed:{
+    rent:300000,
+    buy:9000000,
+  },
+
+}
